018faa89d186242f37b1bac8c1b7d7654524e38a
Victor <mirfaiziev.victor@gmail.com>
2017-06-27 22:41:29 +0200

    added docker

--- app/Console/Commands/CreateSuperAdmin.php
+++ app/Console/Commands/CreateSuperAdmin.php
@@ -19,7 +19,7 @@ class CreateSuperAdmin extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Create superadmin user';
 
     /**
      * Create a new command instance.
